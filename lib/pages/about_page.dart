51e3640dbbb352b30c4f896fe2bed12edeac9417
Matheus Victor <matheusvictor.salles@gmail.com>
2024-01-08 10:25:26 -0300

    feat: add button to Fan Content Policy

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
+import 'package:magic_counter/shared/constants/app_colors.dart';
 import 'package:magic_counter/components/simple_round_button.dart';
 
 class AboutPage extends StatelessWidget {
@@ -12,14 +14,14 @@ class AboutPage extends StatelessWidget {
         name,
         style: const TextStyle(
           fontSize: 16,
-          color: Colors.white,
+          color: Colors.black,
         ),
       ),
       Text(
         value,
         style: const TextStyle(
           fontSize: 16,
-          color: Colors.white,
+          color: Colors.black,
         ),
       )
     ]);
@@ -28,23 +30,38 @@ class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: AppColors.backgroundColor,
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Row(
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Image'),
-              Text(
-                'Magic Counter',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+              Container(
+                width: 90,
+                height: 120,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/logo.png'),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ],
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              top: 20,
+              bottom: 10,
+            ),
+            child: Text(
+              'Magic Counter',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
           ),
           FutureBuilder(
             future: PackageInfo.fromPlatform(),
@@ -53,47 +70,71 @@ class AboutPage extends StatelessWidget {
                 return Text(snapshot.error.toString());
               } else if (snapshot.hasData) {
                 PackageInfo packageInfo = snapshot.data!;
-                return Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      item('App Version', packageInfo.version),
-                    ],
-                  ),
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    item('App Version', packageInfo.version),
+                  ],
                 );
               } else {
                 return const CircularProgressIndicator();
               }
             },
           ),
-          const Column(
+          Column(
             children: [
               Padding(
-                padding: EdgeInsets.only(top: 30),
+                padding: const EdgeInsets.only(top: 10),
                 child: Column(
                   children: [
-                    SimpleRoundButton(
+                    const SimpleRoundButton(
                       buttonText: 'License',
                       width: 200,
                     ),
-                    SimpleRoundButton(
+                    const SimpleRoundButton(
                       buttonText: 'Buy me a coffee',
                       width: 200,
                     ),
-                    SimpleRoundButton(
+                    const SimpleRoundButton(
                       buttonText: 'Source code',
                       width: 200,
                     ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      'Developed by \n @mathvictor and @devruso',
-                      style: TextStyle(
-                        color: Colors.white,
+                    const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        'This app is not affiliated with \n Wizards of the Coast LLC.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
                       ),
-                      textAlign: TextAlign.center,
+                    ),
+                    SimpleRoundButton(
+                      buttonText: 'Wizards of the Coast\'s Fan Content Policy',
+                      width: 200,
+                      height: 50,
+                      backgroundColor: AppColors.warningColor,
+                      fontSize: 14,
+                      onPressed: () async {
+                        if (await canLaunchUrl(
+                          Uri(
+                              scheme: 'https',
+                              host: 'company.wizards.com',
+                              path: '/en/legal/fancontentpolicy'),
+                        )) {
+                        } else {
+                          throw 'Could not launch the url';
+                        }
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(
+                        top: 16.0,
+                      ),
+                      child: Text(
+                        'Developed by \n @mathvictor and @devruso',
+                        textAlign: TextAlign.center,
+                      ),
                     ),
                   ],
                 ),
