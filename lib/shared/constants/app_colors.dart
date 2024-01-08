6b92fcdb6c537bcfd990852c268c438f7be09e5f
Matheus Victor <matheusvictor.salles@gmail.com>
2024-01-08 10:12:42 -0300

    feat: create class to constant color values for the app

---
+++ lib/shared/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF2D2F8F);
+  static const secondaryColor = Color.fromARGB(255, 179, 61, 209);
+  static const backgroundColor = Colors.white;
+  static const textColor = Colors.black;
+  static const buttonPrimaryColor = Colors.blueAccent;
+  static const warningColor = Colors.redAccent;
+  //FEB109
+}
